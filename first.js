72f3bd269fc17f9fe224c68449fabae63b597c6c
Biswarup-01 <biswarupbanerjee01@gmail.com>
2024-08-14 19:24:21 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,9 @@
+// const app = require("./App");
+// console.log(app);
+// const arr = [1, 2, 3, 4, 5, 6];
+// arr.filter((item) => {
+//   if (item % 2) console.log(item);
+// });
+
+const fs = require("fs");
+fs.writeFileSync("hello.txt", "hi");
